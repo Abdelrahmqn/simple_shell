0f23ed45c4cd066802c2d833740040e67f0b652a
abdelrahman <abdelrahman@moh.com>
2023-12-14 04:44:42 +0200

    fix execution file

--- exec_command.c
+++ exec_command.c
@@ -2,9 +2,8 @@
 /**
  * exec_command - Executes the command received as input.
  *
- * @cmd: the command line.
- * @argv: argument vector.
- * @envp: environment..
+ *
+ * @argv: The argument vector containing the command and its arguments.
  *
  * Return: 0 on successful execution, -1 on failure.
  */
@@ -14,33 +13,41 @@ int exec_command(char **cmd, char **argv, char **envp)
 	pid_t child_pid;
 	int status;
 	char *path;
+	(void)argv;
 
 	child_pid = fork();
+	if (child_pid == -1)
+	{
+		perror("Failed to fork");
+		return (-1);
+	}
 
 	if (child_pid == 0)
 	{
 		path = full_path(cmd[0]);
 		if (path == NULL)
 		{
-			perror(argv[0]);
-			_free3(cmd);
-			exit(0);
+			perror("FULL RESOLUTION FAILED");
+			exit(EXIT_FAILURE);
 		}
-		else
-		{
 			if (execve(path, cmd, envp) == -1)
 			{
-				perror(argv[0]);
-				exit(0);
+				perror("EXECUTION FAILED");
+				exit(EXIT_FAILURE);
 			}
-		}
 	}
 
 	else
 	{
-		perror("failed to fork");
-		_free3(cmd);
-		exit(0);
-	}
-	return (WEXITSTATUS(status));
+		waitpid(child_pid, &status, 0);
+		if (WEXITSTATUS(status))
+		{
+			return (WEXITSTATUS(status));
+		}
+			else
+			{
+				return (-1);
+			}
+		}
+	return (-1);
 }
